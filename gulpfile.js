5dc9127de5e9c41f6bf4c08ad4a96a11e5d3dbb4
kleinfreund <phrudloff@gmail.com>
2014-07-30 19:09:49 +0200

    Updating gulpfile + sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,5 @@
-// Include gulp
+// Include gulp + plugins
 var gulp      = require('gulp');
-
-// Include plugins
 var sass      = require('gulp-sass');
 var prefix    = require('gulp-autoprefixer');
 var minifycss = require('gulp-minify-css');
@@ -9,18 +7,49 @@ var rename    = require('gulp-rename');
 
 
 
+// Options
+var options = {};
+
+options.sass = {
+  errLogToConsole: true,
+  sourceMap: 'sass',
+  sourceComments: 'map'
+};
+
+options.autoprefixer = {
+  map: true,
+  from: 'css',
+  to: 'bbkv.min.css'
+};
+
+
+
+// work around for sourcemaps: https://github.com/dlmanning/gulp-sass/issues/28#issuecomment-43951089
+var processWinPath = function(file) {
+    var path = require('path');
+    if (process.platform === 'win32') {
+        file.path = path.relative('.', file.path);
+        file.path = file.path.replace(/\\/g, '/');
+    }
+};
+
+
+
 // Process Sass (compile, prefix, minify)
 gulp.task('sass', function() {
     return gulp.src('sass/**/*.scss')
+        // work around for sourcemaps: https://github.com/dlmanning/gulp-sass/issues/28#issuecomment-43951089
+        .on('data', processWinPath)
+
         // Process Sass files
-        .pipe(sass({
-            // sourceMap: 'sass',
-            // sourceComments: 'map',
-            errLogToConsole: true
-        }))
+        .pipe(sass(options.sass))
 
         // Prefix CSS properties
-        .pipe(prefix('last 2 version', '> 1%', 'ie 9', 'ie 8'))
+        // Autoprefixer doesn't play well with source maps
+        .pipe(prefix(
+            'last 2 version', '> 1%', 'ie 9', 'ie 8',
+            options.autoprefixer
+        ))
 
         // Output regular `*.css`
         .pipe(gulp.dest('css'))
