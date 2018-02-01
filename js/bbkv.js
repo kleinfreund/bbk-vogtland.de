4b051435a370def2603b998020171f1c61ff3a60
kleinfreund <phrudloff@gmail.com>
2018-02-01 10:20:40 +0100

    Start carving out jQuery

--- js/bbkv.js
+++ js/bbkv.js
@@ -1,28 +1,29 @@
-$(document).ready(function(){
-    var $animateTime = 500;
+document.addEventListener('DOMContentLoaded', function () {
+  const animateTime = 500;
 
-    $('.members').on('click', '.member__link', function(event) {
-        event.preventDefault();
+  const sectionLinks = Array.from(document.querySelectorAll('.member__link'));
+  sectionLinks.forEach(sectionLink => sectionLink.addEventListener('click', function (event) {
+    event.preventDefault();
 
-        var $memberContent = $(this).next('.member__content');
+    const sections = document.querySelectorAll('.member__content.visible');
+    sections.forEach(section => section.classList.remove('visible'));
 
-        var $justShown = $memberContent.filter('.visible').removeClass('visible');
+    const currentSection = sectionLink.nextElementSibling;
+    currentSection.classList.add('visible');
 
-        $memberContent.not($justShown).addClass('visible');
-
-        if($memberContent.height() === 0){
-            autoHeightAnimate($memberContent, $animateTime);
-        } else {
-            $memberContent.stop().animate({ height: '0' }, $animateTime);
-        }
-    });
-
-    function autoHeightAnimate(element, time){
-        var $curHeight = element.outerHeight(),
-            $autoHeight = element.css('height', 'auto').outerHeight();
-
-        element.outerHeight($curHeight);
-
-        element.stop().animate({ height: $autoHeight }, parseInt(time));
+    if (currentSection.height() === 0) {
+      autoHeightAnimate(currentSection, animateTime);
+    } else {
+      currentSection.stop().animate({ height: '0' }, animateTime);
     }
+  }));
+
+  function autoHeightAnimate(element, time) {
+    const currentHeight = element.outerHeight();
+    const autoHeight = element.css('height', 'auto').outerHeight();
+
+    element.outerHeight(currentHeight);
+
+    element.stop().animate({ height: autoHeight }, parseInt(time));
+  }
 });
