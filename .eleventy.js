f1283e3c9f98e53da2c4688ebdc97e3a4acf3c7f
kleinfreund <philipprudloff@fastmail.com>
2020-12-30 13:58:57 +0100

    Organize project content in src directory

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,7 @@
+const path = require('path');
+const fs = require('fs');
+
+const CleanCSS = require('clean-css');
 const htmlMinifier = require('html-minifier');
 
 // https://github.com/kangax/html-minifier#options-quick-reference
@@ -12,10 +16,14 @@ module.exports = function (eleventyConfig) {
 
   // Copies static files as they are to the output directory
   eleventyConfig
-    .addPassthroughCopy('static')
-    .addPassthroughCopy('img')
-    .addPassthroughCopy('css')
-    .addPassthroughCopy('.htaccess');
+    .addPassthroughCopy('src/static')
+    .addPassthroughCopy('src/img')
+    .addPassthroughCopy('src/css')
+    .addPassthroughCopy('src/.htaccess');
+
+  // Filter for compressing CSS/JS
+  eleventyConfig.addFilter('resolve_css_imports', resolveCssImports);
+  eleventyConfig.addFilter('minify_css', minifyCss);
 
   // Defines shortcode for generating post excerpts
   eleventyConfig.addShortcode('excerpt', post => extractExcerpt(post));
@@ -26,9 +34,53 @@ module.exports = function (eleventyConfig) {
   }
 
   return {
+    dir: {
+      input: 'src',
+      // Make the project directory the includes directory. This allows me to include files from
+      // across the project instead of just a dedicated includes directory.
+      includes: ''
+    },
     templateFormats: ['md', 'liquid', 'html']
   };
 };
+
+/**
+ * @param {string} mainCssPath
+ * @returns {string}
+ */
+function resolveCssImports(mainCssPath) {
+  const mainCssContent = fs.readFileSync(path.join('src', mainCssPath), 'utf8');
+  const importRules = mainCssContent.split('\n').filter(line => line.startsWith('@import'));
+  const importPaths = importRules.map(importRule => {
+    return path.join('src', importRule.replace('@import \'', '').replace('\';', ''));
+  });
+
+  let concatenatedCssContent = '';
+  for (const importPath of importPaths) {
+    concatenatedCssContent += fs.readFileSync(importPath, 'utf8');
+  }
+
+  return concatenatedCssContent;
+}
+
+/**
+ * Minifies CSS content.
+ *
+ * @param {string} concatenatedCssContent
+ * @returns {string} the minified CSS content
+ */
+function minifyCss(concatenatedCssContent) {
+  const minifyResult = new CleanCSS().minify(concatenatedCssContent);
+
+  if (minifyResult.errors.length > 0) {
+    console.error('❌ Could not minify CSS.');
+    minifyResult.errors.forEach(error => { console.error('❌', error) });
+
+    return concatenatedCssContent;
+  }
+
+  return minifyResult.styles;
+}
 
 /**
  * Minifies HTML content.
